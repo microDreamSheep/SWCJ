b709194fd9c1d73e5c60a404edf4f8081d80dd66
midreamsheep <2493853707@qq.com>
2022-01-24 11:02:12 +0800

    结构优化

--- src/main/java/com/midream/sheep/SWCJ/util/xml/ReptilesBuilder.java
+++ src/main/java/com/midream/sheep/SWCJ/util/xml/ReptilesBuilder.java
@@ -44,26 +44,25 @@ public class ReptilesBuilder implements ReptilesBuilderInter {
             //拼接类
             StringBuilder sb = new StringBuilder();
             //增加包名
-            sb.append("package " + Constant.DEFAULT_PACKAGE_NAME + ";\n");
+            sb.append("package ").append(Constant.DEFAULT_PACKAGE_NAME).append(";\n");
             //拼接类名
-            sb.append("public class " + name + " implements " + rr.getParentInter() + " {");
+            sb.append("public class ").append(name).append(" implements ").append(rr.getParentInter()).append(" {");
             //拼接方法
             {
                 //搭建全局静态属性
                 {
                     //timeout
-                    sb.append("private static int timeout = " + rc.getTimeout() + ";\n");
+                    sb.append("private static int timeout = ").append(rc.getTimeout()).append(";\n");
                     //userAgent数组创建
                     StringBuffer usreAgent = new StringBuffer();
                     usreAgent.append("private static String[] userAgent = new String[]{");
                     for (int i = 0; i < rc.getUserAgents().size(); i++) {
-                        usreAgent.append("\"" + rc.getUserAgents().get(i) + "\"" + ((i + 1 != rc.getUserAgents().size()) ? "," : "};"));
+                        usreAgent.append("\"").append(rc.getUserAgents().get(i)).append("\"").append((i + 1 != rc.getUserAgents().size()) ? "," : "};");
                     }
-                    sb.append(usreAgent.toString() + "\n");
+                    sb.append(usreAgent.toString()).append("\n");
                 }
                 //方法头 定义被重写
-                sb.append("\npublic " + ((rr.getReturnType().equals("String") || rr.getReturnType().equals("java.lang.String")) ? "String" : rr.getReturnType()) + " " + s1 + "(" + (rr.getInPutType().equals("") ? "" : rr.getReturnType() + " " + rr.getInPutName()) + "){" + "\n" +
-                        "try{");
+                sb.append("\npublic ").append((rr.getReturnType().equals("String") || rr.getReturnType().equals("java.lang.String")) ? "String" : rr.getReturnType()).append(" ").append(s1).append("(").append(rr.getInPutType().equals("") ? "" : rr.getReturnType() + " " + rr.getInPutName()).append("){").append("\n").append("try{");
                 //搭建局部变量
                 {
                     if (!(rr.getCookies().equals("")) && !(rr.getCookies() == null)) {
@@ -80,7 +79,7 @@ public class ReptilesBuilder implements ReptilesBuilderInter {
                         sb.append("java.util.Map<String,String> map = new java.util.HashMap<>();");
                         //开始注入cookie值
                         for (Map.Entry<String, String> stringStringEntry : map.entrySet()) {
-                            sb.append("map.put(\"" + stringStringEntry.getKey() + "\",\"" + stringStringEntry.getValue() + "\");\n");
+                            sb.append("map.put(\"").append(stringStringEntry.getKey()).append("\",\"").append(stringStringEntry.getValue()).append("\");\n");
                         }
                     }
                 }
@@ -89,13 +88,10 @@ public class ReptilesBuilder implements ReptilesBuilderInter {
                     //获取方法主体的类
                     ReptileUrl ru = rr.getRu();
                     String map = (!rr.getCookies().equals("") || rr.getCookies() != null) ? ".cookies(map)" : "";
-                    sb.append("\norg.jsoup.nodes.Document document = org.jsoup.Jsoup.connect(\"" + ru.getUrl() + "\").ignoreContentType(true).timeout(timeout)\n"
-                            + map + ".userAgent(userAgent[(int) (Math.random()*userAgent.length)])."
-                            + (ru.getRequestType().equals("POST") ? "post" : "get") + "();");
+                    sb.append("\norg.jsoup.nodes.Document document = org.jsoup.Jsoup.connect(\"").append(ru.getUrl()).append("\").ignoreContentType(true).timeout(timeout)\n").append(map).append(".userAgent(userAgent[(int) (Math.random()*userAgent.length)]).").append(ru.getRequestType().equals("POST") ? "post" : "get").append("();");
                     if (ru.getReg() != null && !ru.getReg().equals("")) {
                         //进入正则表达式方法
-                        sb.append("String text = document." + (ru.isHtml() ? "html" : "text") + "();\n" + "java.util.regex.Pattern pattern = java.util.regex.Pattern.compile(\"" + ru.getReg() + "\");\n" +
-                                "java.util.regex.Matcher matcher = pattern.matcher(text);\n");
+                        sb.append("String text = document.").append(ru.isHtml() ? "html" : "text").append("();\n").append("java.util.regex.Pattern pattern = java.util.regex.Pattern.compile(\"").append(ru.getReg()).append("\");\n").append("java.util.regex.Matcher matcher = pattern.matcher(text);\n");
                         if (rr.getReturnType().equals("String") || rr.getReturnType().equals("java.lang.String") || rr.getReturnType().equals("")) {
                             sb.append("matcher.find();\n" +
                                     "String result =  matcher.group();\n");
@@ -113,18 +109,17 @@ public class ReptilesBuilder implements ReptilesBuilderInter {
                             //一级查询
                             ReptilePaJsoup rpj = ru.getJsoup().get(0);
                             sb.append("\njava.util.List/*<String>*/ list = new java.util.ArrayList/*<>*/();\n");
-                            sb.append("org.jsoup.select.Elements select = document.select(\"" + rpj.getPaText() + "\");\n" +
-                                    "for (int i = 0;i<select.size();i++) {\norg.jsoup.nodes.Element element = select.get(i);");
+                            sb.append("org.jsoup.select.Elements select = document.select(\"").append(rpj.getPaText()).append("\");\n").append("for (int i = 0;i<select.size();i++) {\norg.jsoup.nodes.Element element = select.get(i);");
                             //开始循环
                             String string = "element";//命名空间
                             String end = "element";
                             for (int i = 1; i < ru.getJsoup().size(); i++) {
-                                sb.append("org.jsoup.select.Elements element" + i + " = " + string + ".select(\"" + ru.getJsoup().get(i).getPaText() + "\");\n");
-                                sb.append("for(int a = 0;a<element" + i + ".size();a++) {\norg.jsoup.nodes.Element element" + (i + 1) + " = element" + i + ".get(a);");
+                                sb.append("org.jsoup.select.Elements element").append(i).append(" = ").append(string).append(".select(\"").append(ru.getJsoup().get(i).getPaText()).append("\");\n");
+                                sb.append("for(int a = 0;a<element").append(i).append(".size();a++) {\norg.jsoup.nodes.Element element").append(i + 1).append(" = element").append(i).append(".get(a);");
                                 string = "element" + (i + 1);
                                 end = string;
                             }
-                            sb.append("list.add(" + end + (ru.isHtml() ? ".html" : ".text") + "());\n");
+                            sb.append("list.add(").append(end).append(ru.isHtml() ? ".html" : ".text").append("());\n");
                             //添加括号
                             for (int i = 0; i < ru.getJsoup().size(); i++) {
                                 sb.append("}\n");
@@ -139,23 +134,26 @@ public class ReptilesBuilder implements ReptilesBuilderInter {
             sb.append("}catch (Exception e){\ne.printStackTrace();\n}\nreturn null;\n}");
             //类封口
             sb.append("\n}");
+            //实例化文件类
+            File javaFile = new File(rc.getWorkplace() + "//" + name + ".java");
             //输出到工作空间
-            sio.outPutString(sb.toString(), new File(rc.getWorkplace() + "//" + name + ".java"));
+            sio.outPutString(sb.toString(), javaFile);
             //编译类
-            String s = swcjcl.compileJavaFile(new File(rc.getWorkplace() + "//" + name + ".java"));
+            String s = swcjcl.compileJavaFile(javaFile);
+            //实例化文件类
+            File classFile = new File(s);
+            //加载类
             Class<?> aClass = swcjcl.loadData(Constant.DEFAULT_PACKAGE_NAME + "." + name, s);
             if (rc.isCache()) {
-                new File(rc.getWorkplace() + "//" + name + ".java").exists();
-                new File(s).exists();
+                javaFile.delete();
+                classFile.delete();
             }
             if (aClass != null) {
                 //返回类
                 return aClass.getDeclaredConstructor().newInstance();
             }
         } catch (Exception e) {
-            System.out.println("异常");
             e.printStackTrace();
-        } finally {
         }
         return null;
     }
