2942cc109fa9f8efc2de9d3562454ad4cf4b0714
midreamsheep <2493853707@qq.com>
2022-04-30 14:59:05 +0800

    还有一个月中考，祝福我吧

--- src/main/java/com/midream/sheep/swcj/util/compiler/SWCJCompiler.java
+++ src/main/java/com/midream/sheep/swcj/util/compiler/SWCJCompiler.java
@@ -1,6 +1,10 @@
 package com.midream.sheep.swcj.util.compiler;
 
+import com.midream.sheep.swcj.pojo.SWCJClass;
+
 public interface SWCJCompiler {
     //编译类
     Class<?> compileAndLoad(String fullName, String sourceCode) throws ClassNotFoundException;
+    //拼接类
+
 }
---
+++ src/main/java/com/midream/sheep/swcj/util/compiler/javassist/SWCJJavassist.java
@@ -0,0 +1,13 @@
+package com.midream.sheep.swcj.util.compiler.javassist;
+
+import com.midream.sheep.swcj.util.compiler.SWCJCompiler;
+
+/**
+ * @author midreamsheep
+ */
+public class SWCJJavassist {
+    public Class<?> compileAndLoad(String fullName, String sourceCode) throws ClassNotFoundException {
+
+        return null;
+    }
+}
