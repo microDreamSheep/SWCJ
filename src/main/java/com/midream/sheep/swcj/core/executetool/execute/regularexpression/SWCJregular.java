8fa4deac6f6eb936383cb2ef00e81995909365c2
microDreamSheep <atthemc313128460@gmail.com>
2022-11-09 09:15:32 +0800

    重构正则执行器

--- src/main/java/com/midream/sheep/swcj/core/executetool/execute/regularexpression/SWCJregular.java
+++ src/main/java/com/midream/sheep/swcj/core/executetool/execute/regularexpression/SWCJregular.java
@@ -6,15 +6,12 @@ import com.midream.sheep.swcj.core.executetool.SWCJExecute;
 import com.midream.sheep.swcj.data.Constant;
 import com.midream.sheep.swcj.data.XmlSpecialStrings;
 import com.midream.sheep.swcj.pojo.ExecuteValue;
-import com.midream.sheep.swcj.util.function.StringUtil;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
-import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.*;
